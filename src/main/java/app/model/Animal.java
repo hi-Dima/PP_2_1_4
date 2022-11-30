f2f18aa5200abe96fedbec4f654820fe060eb5cb
DmitriyS <eastmoscow@mail.ru>
2022-11-30 14:18:22 +0300

    pp4

---
+++ src/main/java/app/model/Animal.java
@@ -0,0 +1,7 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component ("animal")
+public abstract class Animal {
+}
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,7 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component ("dog")
+public class Dog extends Animal {
+}
